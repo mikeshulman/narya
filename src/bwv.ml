b1d271fa94116051fa81389bc39de4d39ebd2572
Michael Shulman <shulman@sandiego.edu>
2023-08-15 13:17:47 -0700

    hlist and mmap for bwv

--- src/bwv.ml
+++ src/bwv.ml
@@ -1,3 +1,5 @@
+open Hlist
+
 (* Snoc vectors, indexed by type-level natural numbers.  This module should not be opened, but used qualified. *)
 
 (* An ('a, 'n) Bwv.t is a vector of length 'n of elements of type 'a. *)
@@ -5,6 +7,13 @@ type (_, _) t = Emp : ('a, N.zero) t | Snoc : ('a, 'n) t * 'a -> ('a, 'n N.suc) 
 
 let emp : type a. (a, N.zero) t = Emp
 let snoc : type a n. (a, n) t -> a -> (a, n N.suc) t = fun xs x -> Snoc (xs, x)
+
+(* A Bwv of nonempty length always has a "tail" (now, the last element) and "head" (the rest of the Bwv), with no possibility of exceptions. *)
+let tail : type a n. (a, n N.suc) t -> a = function
+  | Snoc (_, x) -> x
+
+let head : type a n. (a, n N.suc) t -> (a, n) t = function
+  | Snoc (xs, _) -> xs
 
 (* The length of a vector is always a natural number. *)
 let rec zero_plus_length : type a n. (a, n) t -> (N.zero, n, n) N.plus = function
@@ -63,65 +72,124 @@ let rec index : type a n. a -> (a, n) t -> n N.index option =
   | Emp -> None
   | Snoc (xs, x) -> if x = y then Some Top else Option.map (fun z -> N.Pop z) (index y xs)
 
-(* Mapping and iterating over vectors *)
+(* Mapping and iterating over vectors.  We use the general framework of mmap for traversals, so we start with heterogeneous lists. *)
 
-let rec map : type a b n. (a -> b) -> (a, n) t -> (b, n) t =
- fun f -> function
-  | Emp -> Emp
-  | Snoc (xs, x) -> Snoc (map f xs, f x)
+module Heter = struct
+  (* An hlist of bwvs, all of the same length. *)
+  type (_, _) ht =
+    | [] : (nil, 'n) ht
+    | ( :: ) : ('x, 'n) t * ('xs, 'n) ht -> (('x, 'xs) cons, 'n) ht
 
-let rec map_plus : type a b m n mn. (m, n, mn) N.plus -> (a -> b) -> (a, mn) t -> (b, m) t =
- fun mn f xs ->
-  match mn with
-  | Zero -> map f xs
-  | Suc mn -> (
-      match xs with
-      | Snoc (xs, _) -> map_plus mn f xs)
+  (* The hlist of empty bwvs. *)
+  let rec emp : type xs. xs tlist -> (xs, N.zero) ht = function
+    | Nil -> []
+    | Cons xs -> Emp :: emp xs
 
-let rec map2 : type a b c n. (a -> b -> c) -> (a, n) t -> (b, n) t -> (c, n) t =
- fun f xs ys ->
-  match (xs, ys) with
-  | Emp, Emp -> Emp
-  | Snoc (xs, x), Snoc (ys, y) -> Snoc (map2 f xs ys, f x y)
+  (* Add an element to the end of each of an hlist of bwvs. *)
+  let rec snoc : type xs n. (xs, n) ht -> xs hlist -> (xs, n N.suc) ht =
+   fun xs ys ->
+    match (xs, ys) with
+    | [], [] -> []
+    | x :: xs, y :: ys -> Snoc (x, y) :: snoc xs ys
 
-let rec map3 : type a b c d n. (a -> b -> c -> d) -> (a, n) t -> (b, n) t -> (c, n) t -> (d, n) t =
- fun f xs ys zs ->
-  match (xs, ys, zs) with
-  | Emp, Emp, Emp -> Emp
-  | Snoc (xs, x), Snoc (ys, y), Snoc (zs, z) -> Snoc (map3 f xs ys zs, f x y z)
+  let head' = head
+  let tail' = tail
 
-let rec map2_plus :
-    type a b c m n mn. (m, n, mn) N.plus -> (a -> b -> c) -> (a, mn) t -> (b, m) t -> (c, m) t =
- fun mn f xs ys ->
-  match mn with
-  | Zero -> map2 f xs ys
-  | Suc mn -> (
-      match xs with
-      | Snoc (xs, _) -> map2_plus mn f xs ys)
+  (* Extract the head and tail of each of an hlist of bwvs, returting another hlist.*)
+  let rec tail : type xs n. (xs, n N.suc) ht -> xs hlist = function
+    | [] -> []
+    | x :: xs -> tail' x :: tail xs
 
-let rec iter : type a n. (a -> unit) -> (a, n) t -> unit =
- fun f -> function
-  | Emp -> ()
-  | Snoc (xs, x) ->
-      iter f xs;
-      f x
+  let rec head : type xs n. (xs, n N.suc) ht -> (xs, n) ht = function
+    | [] -> []
+    | x :: xs -> head' x :: head xs
+end
 
-let rec iter2 : type a b n. (a -> b -> unit) -> (a, n) t -> (b, n) t -> unit =
- fun f xs ys ->
-  match (xs, ys) with
-  | Emp, Emp -> ()
-  | Snoc (xs, x), Snoc (ys, y) ->
-      iter2 f xs ys;
-      f x y
+(* Now we can define the general monadic heterogeneous traversal. *)
 
-let rec iter2_plus :
-    type a b m n mn. (m, n, mn) N.plus -> (a -> b -> unit) -> (a, mn) t -> (b, m) t -> unit =
- fun mn f xs ys ->
-  match mn with
-  | Zero -> iter2 f xs ys
-  | Suc mn -> (
-      match xs with
-      | Snoc (xs, _) -> iter2_plus mn f xs ys)
+module Monadic (M : Monad.Plain) = struct
+  open Monad.Ops (M)
+
+  let rec pmapM :
+      type x xs ys n.
+      ((x, xs) cons hlist -> ys hlist M.t) ->
+      ((x, xs) cons, n) Heter.ht ->
+      ys tlist ->
+      (ys, n) Heter.ht M.t =
+   fun f xss ys ->
+    match xss with
+    | Emp :: _ -> return (Heter.emp ys)
+    | Snoc (xs, x) :: xss ->
+        let* fxs = pmapM f (xs :: Heter.head xss) ys in
+        let* fx = f (x :: Heter.tail xss) in
+        return (Heter.snoc fxs fx)
+
+  (* With specializations to simple arity possibly-monadic maps and iterators.  *)
+
+  let miterM :
+      type x xs n. ((x, xs) cons hlist -> unit M.t) -> ((x, xs) cons, n) Heter.ht -> unit M.t =
+   fun f xss ->
+    let* [] =
+      pmapM
+        (fun x ->
+          let* () = f x in
+          return [])
+        xss Nil in
+    return ()
+
+  let mmapM :
+      type x xs y n. ((x, xs) cons hlist -> y M.t) -> ((x, xs) cons, n) Heter.ht -> (y, n) t M.t =
+   fun f xss ->
+    let* [ ys ] =
+      pmapM
+        (fun x ->
+          let* y = f x in
+          return [ y ])
+        xss (Cons Nil) in
+    return ys
+
+  let mapM : type x y n. (x -> y M.t) -> (x, n) t -> (y, n) t M.t =
+   fun f xs -> mmapM (fun [ x ] -> f x) [ xs ]
+
+  let mapM2 : type x y z n. (x -> y -> z M.t) -> (x, n) t -> (y, n) t -> (z, n) t M.t =
+   fun f xs ys -> mmapM (fun [ x; y ] -> f x y) [ xs; ys ]
+
+  let iterM : type x n. (x -> unit M.t) -> (x, n) t -> unit M.t =
+   fun f xs -> miterM (fun [ x ] -> f x) [ xs ]
+
+  let iterM2 : type x y n. (x -> y -> unit M.t) -> (x, n) t -> (y, n) t -> unit M.t =
+   fun f xs ys -> miterM (fun [ x; y ] -> f x y) [ xs; ys ]
+end
+
+let pmap :
+    type x xs ys n.
+    ((x, xs) cons hlist -> ys hlist) -> ((x, xs) cons, n) Heter.ht -> ys tlist -> (ys, n) Heter.ht =
+ fun f xss ys ->
+  let open Monadic (Monad.Identity) in
+  pmapM f xss ys
+
+let mmap : type x xs y n. ((x, xs) cons hlist -> y) -> ((x, xs) cons, n) Heter.ht -> (y, n) t =
+ fun f xs ->
+  let open Monadic (Monad.Identity) in
+  mmapM f xs
+
+let miter : type x xs n. ((x, xs) cons hlist -> unit) -> ((x, xs) cons, n) Heter.ht -> unit =
+ fun f xss ->
+  let open Monadic (Monad.Identity) in
+  miterM f xss
+
+let map : type a b n. (a -> b) -> (a, n) t -> (b, n) t = fun f xs -> mmap (fun [ x ] -> f x) [ xs ]
+
+let map2 : type a b c n. (a -> b -> c) -> (a, n) t -> (b, n) t -> (c, n) t =
+ fun f xs ys -> mmap (fun [ x; y ] -> f x y) [ xs; ys ]
+
+(* Note that with these definitions, the iteration direction is from *left to right*, in contrast to the choice made for backwards lists in Bwd. *)
+let iter : type a n. (a -> unit) -> (a, n) t -> unit = fun f xs -> miter (fun [ x ] -> f x) [ xs ]
+
+let iter2 : type a b n. (a -> b -> unit) -> (a, n) t -> (b, n) t -> unit =
+ fun f xs ys -> miter (fun [ x; y ] -> f x y) [ xs; ys ]
+
+(* Searching *)
 
 (* Find an element in one Bwv satisfying a predicate, if one exists, and return a function of the element and length of the remaining vector at the same (backwards) index of another, possibly longer, Bwv. *)
 let rec find2l :
@@ -132,6 +200,8 @@ let rec find2l :
   | Zero, _, _ -> None
   | Suc mn, Snoc (xs, x), Snoc (ys, y) ->
       if p x then Some (f (N.to_int (length ys)) y) else find2l mn p f xs ys
+
+(* Appending *)
 
 (* Amusingly, appending *reversed* vectors is a closer match to addition of natural numbers defined by recursion on its right argument that appending non-reversed vectors would be. *)
 let rec append : type a m n mn. (m, n, mn) N.plus -> (a, m) t -> (a, n) t -> (a, mn) t =
@@ -153,6 +223,8 @@ let rec unappend : type a m n mn. (m, n, mn) N.plus -> (a, mn) t -> (a, m) t * (
 
 (* Folds *)
 
+(* As noted in mlist, these could be defined in terms of the monadic iterator iterM using the state and continuation monads. *)
+
 let rec fold_left : type n a b. (a -> b -> a) -> a -> (b, n) t -> a =
  fun f start xs ->
   match xs with
@@ -165,19 +237,32 @@ let rec fold_right : type n a b. (a -> b -> b) -> (a, n) t -> b -> b =
   | Emp -> start
   | Snoc (xs, x) -> fold_right f xs (f x start)
 
-let rec fold2_left : type n a b c. (a -> b -> c -> a) -> a -> (b, n) t -> (c, n) t -> a =
+let rec fold_left2 : type n a b c. (a -> b -> c -> a) -> a -> (b, n) t -> (c, n) t -> a =
  fun f start xs ys ->
   match (xs, ys) with
   | Emp, Emp -> start
-  | Snoc (xs, x), Snoc (ys, y) -> f (fold2_left f start xs ys) x y
+  | Snoc (xs, x), Snoc (ys, y) -> f (fold_left2 f start xs ys) x y
 
-(* A version of fold2_left where the accumulator type 'a is itself replaced by a vector, which starts at length m and results of length m+n.  This requires that the function being applied at each step is polymorphic over the length of the vector, and hence it must be wrapped in a record.  *)
-type ('a, 'b, 'c) fold2_left_mapper = { f : 'n. ('a, 'n) t -> 'b -> 'c -> 'a }
+(* A version of fold_left where the accumulator type 'a is itself replaced by a vector, which starts at length m and results of length m+n.  This requires that the function being applied at each step is polymorphic over the length of the vector, and hence it must be wrapped in a record.  *)
 
-let rec fold2_left_map_append :
+type ('a, 'b) fold_left_mapper = { fold : 'n. ('a, 'n) t -> 'b -> 'a }
+
+let rec fold_left_map_append :
+    type m n mn a b c.
+    (m, n, mn) N.plus -> (a, b) fold_left_mapper -> (a, m) t -> (b, n) t -> (a, mn) t =
+ fun mn f start xs ->
+  match (mn, xs) with
+  | Zero, Emp -> start
+  | Suc mn, Snoc (xs, x) ->
+      let zs = fold_left_map_append mn f start xs in
+      Snoc (zs, f.fold zs x)
+
+type ('a, 'b, 'c) fold_left2_mapper = { fold : 'n. ('a, 'n) t -> 'b -> 'c -> 'a }
+
+let rec fold_left2_map_append :
     type m n mn a b c.
     (m, n, mn) N.plus ->
-    (a, b, c) fold2_left_mapper ->
+    (a, b, c) fold_left2_mapper ->
     (a, m) t ->
     (b, n) t ->
     (c, n) t ->
@@ -186,8 +271,8 @@ let rec fold2_left_map_append :
   match (mn, xs, ys) with
   | Zero, Emp, Emp -> start
   | Suc mn, Snoc (xs, x), Snoc (ys, y) ->
-      let zs = fold2_left_map_append mn f start xs ys in
-      Snoc (zs, f.f zs x y)
+      let zs = fold_left2_map_append mn f start xs ys in
+      Snoc (zs, f.fold zs x y)
 
 (* Constant-length bind: given a vector of elements of 'a of length n, and a function mapping 'a to vectors of type 'b of length m, we get a vector of type 'b of length m*n.  *)
 let rec bind : type a b m n mn. (m, n, mn) N.times -> (a, n) t -> (a -> (b, m) t) -> (b, mn) t =
@@ -195,6 +280,47 @@ let rec bind : type a b m n mn. (m, n, mn) N.times -> (a, n) t -> (a -> (b, m) t
   match (mn, xs) with
   | Zero _, Emp -> Emp
   | Suc (mn, mnm), Snoc (xs, x) -> append mnm (bind mn xs f) (f x)
+
+let rec fold_left_bind_append :
+    type k m n mn k_mn a c.
+    (m, n, mn) N.times ->
+    (k, mn, k_mn) N.plus ->
+    (a, c) fold_left_mapper ->
+    (a, k) t ->
+    ((c, m) t, n) t ->
+    (a, k_mn) t =
+ fun mn k_mn f acc xs ->
+  match (mn, xs) with
+  | Zero _, Emp ->
+      let Eq = N.plus_uniq k_mn Zero in
+      acc
+  | Suc (mn', mn'_m), Snoc (xs, x) ->
+      let (Plus k_mn') = N.plus (N.times_out mn') in
+      let k_mn'__m = N.plus_assocl k_mn' mn'_m k_mn in
+      fold_left_map_append k_mn'__m f (fold_left_bind_append mn' k_mn' f acc xs) x
+
+type ('a, 'b, 'c, 'm) fold_left2_bind_appender = {
+  append : 'n 'nm. ('n, 'm, 'nm) N.plus -> ('a, 'n) t -> 'b -> 'c -> ('a, 'nm) t;
+}
+
+let rec fold_left2_bind_append :
+    type k m n mn k_mn a b c.
+    (m, n, mn) N.times ->
+    (k, mn, k_mn) N.plus ->
+    (a, k) t ->
+    (b, n) t ->
+    (c, n) t ->
+    (a, b, c, m) fold_left2_bind_appender ->
+    (a, k_mn) t =
+ fun mn k_mn acc xs ys g ->
+  match (mn, xs, ys) with
+  | Zero _, Emp, Emp ->
+      let Eq = N.plus_uniq k_mn Zero in
+      acc
+  | Suc (mn', mn'_m), Snoc (xs, x), Snoc (ys, y) ->
+      let (Plus k_mn') = N.plus (N.times_out mn') in
+      let k_mn'__m = N.plus_assocl k_mn' mn'_m k_mn in
+      g.append k_mn'__m (fold_left2_bind_append mn' k_mn' acc xs ys g) x y
 
 (* Conversely, a vector of length m*n can be split into a length-n vector of length-m vectors. *)
 let rec unbind : type a b m n mn. (m, n, mn) N.times -> (b, mn) t -> ((b, m) t, n) t =
--- src/monad.ml
+++ src/monad.ml
@@ -1,7 +1,5 @@
 (* This module is not meant to be opened; its sub-modules should be used qualified. *)
 
-(* open Bwd *)
-
 (* Plain monads *)
 
 module type Plain = sig
@@ -28,15 +26,6 @@ module Ops (M : Plain) = struct
         let* frest = mapM f xs in
         return (fx :: frest)
 
-  let rec bwv_mapM : type a b n. (a -> b M.t) -> (a, n) Bwv.t -> (b, n) Bwv.t M.t =
-   fun f xs ->
-    match xs with
-    | Emp -> return Bwv.Emp
-    | Snoc (xs, x) ->
-        let* frest = bwv_mapM f xs in
-        let* fx = f x in
-        return (Bwv.Snoc (frest, fx))
-
   let option_mapM (f : 'a -> 'b M.t) : 'a option -> 'b option M.t = function
     | Some x ->
         let* fx = f x in
@@ -57,12 +46,6 @@ module Ops (M : Plain) = struct
         let* rest = list_fold_rightM f xs start in
         f x rest
 
-  (* let queue_fold_leftM (f : 'a -> 'b -> 'a M.t) (start : 'a M.t) ((qout, qin) : 'b Queue.t) : 'a M.t = *)
-  (*   list_fold_rightM (fun x y -> f y x) qin (list_fold_leftM f start qout) *)
-
-  (* let queue_fold_right (f : 'a -> 'b -> 'b M.t) ((qout, qin) : 'a Queue.t) (start : 'b M.t) : 'b M.t = *)
-  (*   list_fold_rightM f qout (list_fold_leftM (fun x y -> f y x) start qin) *)
-
   let rec mapM2 (f : 'a -> 'b -> 'c M.t) (xs : 'a list) (ys : 'b list) : 'c list M.t =
     match (xs, ys) with
     | [], [] -> return []
@@ -72,86 +55,12 @@ module Ops (M : Plain) = struct
         return (fxy :: frest)
     | _, _ -> raise (Invalid_argument "mapM2: lists are of different lengths")
 
-  let rec bwv_mapM2 :
-      type a b c n. (a -> b -> c M.t) -> (a, n) Bwv.t -> (b, n) Bwv.t -> (c, n) Bwv.t M.t =
-   fun f xs ys ->
-    match (xs, ys) with
-    | Bwv.Emp, Bwv.Emp -> return Bwv.Emp
-    | Snoc (xs, x), Snoc (ys, y) ->
-        let* frest = bwv_mapM2 f xs ys in
-        let* fxy = f x y in
-        return (Bwv.Snoc (frest, fxy))
-
-  let rec bwv_mapM3 :
-      type a b c d n.
-      (a -> b -> c -> d M.t) -> (a, n) Bwv.t -> (b, n) Bwv.t -> (c, n) Bwv.t -> (d, n) Bwv.t M.t =
-   fun f xs ys zs ->
-    match (xs, ys, zs) with
-    | Bwv.Emp, Bwv.Emp, Bwv.Emp -> return Bwv.Emp
-    | Snoc (xs, x), Snoc (ys, y), Snoc (zs, z) ->
-        let* frest = bwv_mapM3 f xs ys zs in
-        let* fxy = f x y z in
-        return (Bwv.Snoc (frest, fxy))
-
-  let rec bwv_mapM3_2 :
-      type a b c d e n.
-      (a -> b -> c -> (d * e) M.t) ->
-      (a, n) Bwv.t ->
-      (b, n) Bwv.t ->
-      (c, n) Bwv.t ->
-      ((d, n) Bwv.t * (e, n) Bwv.t) M.t =
-   fun f xs ys zs ->
-    match (xs, ys, zs) with
-    | Bwv.Emp, Bwv.Emp, Bwv.Emp -> return (Bwv.Emp, Bwv.Emp)
-    | Snoc (xs, x), Snoc (ys, y), Snoc (zs, z) ->
-        let* frest1, frest2 = bwv_mapM3_2 f xs ys zs in
-        let* fxy1, fxy2 = f x y z in
-        return (Bwv.Snoc (frest1, fxy1), Bwv.Snoc (frest2, fxy2))
-
-  let rec bwv_mapM3_plus :
-      type a b c d m n mn.
-      (m, n, mn) N.plus ->
-      (a -> b -> c -> d M.t) ->
-      (a, mn) Bwv.t ->
-      (b, mn) Bwv.t ->
-      (c, m) Bwv.t ->
-      (d, m) Bwv.t M.t =
-   fun mn f xs ys zs ->
-    match mn with
-    | Zero -> bwv_mapM3 f xs ys zs
-    | Suc mn -> (
-        match (xs, ys) with
-        | Snoc (xs, _), Snoc (ys, _) -> bwv_mapM3_plus mn f xs ys zs)
-
-  let rec bwv_mapM3_2_plus :
-      type a b c d e m n mn.
-      (m, n, mn) N.plus ->
-      (a -> b -> c -> (d * e) M.t) ->
-      (a, mn) Bwv.t ->
-      (b, mn) Bwv.t ->
-      (c, m) Bwv.t ->
-      ((d, m) Bwv.t * (e, m) Bwv.t) M.t =
-   fun mn f xs ys zs ->
-    match mn with
-    | Zero -> bwv_mapM3_2 f xs ys zs
-    | Suc mn -> (
-        match (xs, ys) with
-        | Snoc (xs, _), Snoc (ys, _) -> bwv_mapM3_2_plus mn f xs ys zs)
-
   let rec iterM (f : 'a -> unit M.t) (xs : 'a list) : unit M.t =
     match xs with
     | [] -> return ()
     | x :: xs ->
         let* _ = f x in
         iterM f xs
-
-  let rec bwv_iterM : type a n. (a -> unit M.t) -> (a, n) Bwv.t -> unit M.t =
-   fun f xs ->
-    match xs with
-    | Emp -> return ()
-    | Snoc (xs, x) ->
-        let* () = bwv_iterM f xs in
-        f x
 
   let rec iterM2 (f : 'a -> 'b -> unit M.t) (xs : 'a list) (ys : 'b list) : unit M.t =
     match (xs, ys) with
@@ -161,25 +70,6 @@ module Ops (M : Plain) = struct
         iterM2 f xs ys
     | _, _ -> raise (Invalid_argument "iterM2: lists are of different lengths")
 
-  let rec bwv_iterM2 : type a b n. (a -> b -> unit M.t) -> (a, n) Bwv.t -> (b, n) Bwv.t -> unit M.t
-      =
-   fun f xs ys ->
-    match (xs, ys) with
-    | Emp, Emp -> return ()
-    | Snoc (xs, x), Snoc (ys, y) ->
-        let* () = bwv_iterM2 f xs ys in
-        f x y
-
-  let rec bwv_iterM2_plus :
-      type a b m n mn.
-      (m, n, mn) N.plus -> (a -> b -> unit M.t) -> (a, mn) Bwv.t -> (b, m) Bwv.t -> unit M.t =
-   fun mn f xs ys ->
-    match mn with
-    | Zero -> bwv_iterM2 f xs ys
-    | Suc mn -> (
-        match xs with
-        | Snoc (xs, _) -> bwv_iterM2_plus mn f xs ys)
-
   let rec iterM3 (f : 'a -> 'b -> 'c -> unit M.t) (xs : 'a list) (ys : 'b list) (zs : 'c list) :
       unit M.t =
     match (xs, ys, zs) with
@@ -188,31 +78,6 @@ module Ops (M : Plain) = struct
         let* _ = f x y z in
         iterM3 f xs ys zs
     | _, _, _ -> raise (Invalid_argument "iterM3: lists are of different lengths")
-
-  let rec bwv_iterM3 :
-      type a b c n.
-      (a -> b -> c -> unit M.t) -> (a, n) Bwv.t -> (b, n) Bwv.t -> (c, n) Bwv.t -> unit M.t =
-   fun f xs ys zs ->
-    match (xs, ys, zs) with
-    | Emp, Emp, Emp -> return ()
-    | Snoc (xs, x), Snoc (ys, y), Snoc (zs, z) ->
-        let* () = bwv_iterM3 f xs ys zs in
-        f x y z
-
-  let rec bwv_iterM3_plus :
-      type a b c m n mn.
-      (m, n, mn) N.plus ->
-      (a -> b -> c -> unit M.t) ->
-      (a, mn) Bwv.t ->
-      (b, mn) Bwv.t ->
-      (c, m) Bwv.t ->
-      unit M.t =
-   fun mn f xs ys zs ->
-    match mn with
-    | Zero -> bwv_iterM3 f xs ys zs
-    | Suc mn -> (
-        match (xs, ys) with
-        | Snoc (xs, _), Snoc (ys, _) -> bwv_iterM3_plus mn f xs ys zs)
 end
 
 (* The identity monad *)
@@ -331,6 +196,8 @@ module List = struct
 end
 
 (*
+open Bwd
+
 module Bwd = struct
   type 'a t = 'a Bwd.t
 

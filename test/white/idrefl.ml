4368d1230806e875e6c0a293460ea72885fb327b
Michael Shulman <shulman@sandiego.edu>
2023-11-01 16:19:20 -0700

    pmp->mcp in idrefl test

--- test/white/idrefl.ml
+++ test/white/idrefl.ml
@@ -1,166 +1,118 @@
-open Testutil.Pmp
+open Testutil.Mcp
 
-let uu, _ = synth UU
-let xx = assume "X" uu
-let x0 = assume "x0" xx
-let x1 = assume "x1" xx
+let () =
+  run @@ fun () ->
+  let uu, _ = synth "Type" in
+  let xx = assume "X" uu in
+  let x0 = assume "x0" xx in
+  let x1 = assume "x1" xx in
 
-(* Identity types are equivalently instantiations of refl of a type *)
-let idx01, _ = synth (id !!"X" !!"x0" !!"x1")
-let idx01', _ = synth (refl !!"X" $ !!"x0" $ !!"x1")
-let () = equal idx01 idx01'
-let x2 = assume "x2" idx01
-let xtox, _ = synth (("x", !!"X") @=> !!"X")
+  (* Identity types are equivalently instantiations of refl of a type *)
+  let idx01, _ = synth "Id X x0 x1" in
+  let idx01', _ = synth "refl X x0 x1" in
+  equal idx01 idx01';
+  let x2 = assume "x2" idx01 in
+  let xtox, _ = synth "X → X" in
 
-(* The identity function acts as the identity *)
-let idmap = check ("x" @-> !!"x") xtox
-let () = equal (idmap $$ x0) x0
-let () = unequal (idmap $$ x0) x1
-let idmapx0, _ = synth ("x" @-> !!"x" <:> ("x", !!"X") @=> !!"X" $ !!"x0")
-let () = equal idmapx0 x0
-let () = unsynth ("x" @-> !!"x" <:> ("x", !!"X") @=> !!"X" $ !!"x2")
+  (* The identity function acts as the identity *)
+  let idmap = check "x ↦ x" xtox in
+  let idmapx0, _ = synth "((x ↦ x) : X → X) x0" in
+  equal idmapx0 x0;
+  unequal idmapx0 x1;
+  unsynth "((x ↦ x) : X → X) x2";
 
-(* refl of the identity function acts as the identity on identifications *)
-let refl_idmap_x2, _ =
-  synth (refl ("x" @-> !!"x" <:> ("x", !!"X") @=> !!"X") $ !!"x0" $ !!"x1" $ !!"x2")
+  (* refl of the identity function acts as the identity on identifications *)
+  let refl_idmap_x2, _ = synth "refl ((x ↦ x) : (X → X)) x0 x1 x2" in
 
-let () = unsynth (refl ("x" @-> !!"x" <:> ("x", !!"X") @=> !!"X") $ !!"x0" $ !!"x0" $ !!"x2")
-let () = unsynth (refl ("x" @-> !!"x" <:> ("x", !!"X") @=> !!"X") $ !!"x0" $ !!"x1" $ !!"x0")
-let () = equal refl_idmap_x2 x2
+  unsynth "refl ((x ↦ x) : (X → X)) x0 x0 x2";
+  unsynth "refl ((x ↦ x) : (X → X)) x0 x0 x0";
+  equal refl_idmap_x2 x2;
 
-(*  *)
-let yy = assume "Y" uu
-let zz = assume "Z" uu
-let xtoy, _ = synth (("x", !!"X") @=> !!"Y")
-let ytoz, _ = synth (("y", !!"Y") @=> !!"Z")
-let xtoz, _ = synth (("x", !!"X") @=> !!"Z")
-let f = assume "f" xtoy
-let g = assume "g" ytoz
-let gof = check ("x" @-> (!!"g" $ (!!"f" $ !!"x"))) xtoz
-let () = uncheck ("x" @-> (!!"f" $ (!!"g" $ !!"x"))) xtoz
+  (*  *)
+  let yy = assume "Y" uu in
+  let zz = assume "Z" uu in
+  let xtoy, _ = synth "X → Y" in
+  let ytoz, _ = synth "Y → Z" in
+  let xtoz, _ = synth "X → Z" in
+  let f = assume "f" xtoy in
+  let g = assume "g" ytoz in
+  let gof = check "x ↦ g (f x)" xtoz in
+  uncheck "x ↦ f (g x)" xtoz;
 
-(* Identity types of pi-types don't *compute* to the expected thing, but they are isomorphic *)
-let f' = assume "f'" xtoy
-let idff, _ = synth (id (("x", !!"X") @=> !!"Y") !!"f" !!"f'")
+  (* Identity types of pi-types don't *compute* to the expected thing, but they are isomorphic, by eta-expansion in both directions. *)
+  let f' = assume "f'" xtoy in
+  let idff, _ = synth "Id (X → Y) f f'" in
 
-let idff', _ =
-  synth
-    (("x", !!"X")
-    @=> ("x'", !!"X")
-    @=> ("x''", id !!"X" !!"x" !!"x'")
-    @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'"))
+  let idff', _ = synth "(x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x')" in
 
-let () = unequal idff idff'
+  unequal idff idff';
 
-let idff_to_idff'_ty, _ =
-  synth
-    (("", id (("x", !!"X") @=> !!"Y") !!"f" !!"f'")
-    @=> ("x", !!"X")
-    @=> ("x'", !!"X")
-    @=> ("x''", id !!"X" !!"x" !!"x'")
-    @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'"))
+  let idff_to_idff'_ty, _ =
+    synth "Id (X → Y) f f' → (x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x')" in
 
-let idff_to_idff' =
-  check ("h" @-> "x" @-> "x'" @-> "x''" @-> (!!"h" $ !!"x" $ !!"x'" $ !!"x''")) idff_to_idff'_ty
+  let idff_to_idff' = check "h x x' x'' ↦ h x x' x''" idff_to_idff'_ty in
 
-let idff'_to_idff_ty, _ =
-  synth
-    (( "",
-       ("x", !!"X")
-       @=> ("x'", !!"X")
-       @=> ("x''", id !!"X" !!"x" !!"x'")
-       @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'") )
-    @=> id (("x", !!"X") @=> !!"Y") !!"f" !!"f'")
+  let idff'_to_idff_ty, _ =
+    synth "((x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x')) → Id (X → Y) f f'" in
 
-let idff'_to_idff =
-  check ("k" @-> "x" @-> "x'" @-> "x''" @-> (!!"k" $ !!"x" $ !!"x'" $ !!"x''")) idff'_to_idff_ty
+  let idff'_to_idff = check "k x x' x'' ↦ k x x' x''" idff'_to_idff_ty in
 
-let p = assume "p" idff
-let q = assume "q" idff'
+  let p = assume "p" idff in
+  let q = assume "q" idff' in
 
-let idff_roundtrip, _ =
-  synth
-    ("k" @-> "x" @-> "x'" @-> "x''" @-> (!!"k" $ !!"x" $ !!"x'" $ !!"x''")
-    <:> ( "",
-          ("x", !!"X")
-          @=> ("x'", !!"X")
-          @=> ("x''", id !!"X" !!"x" !!"x'")
-          @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'") )
-        @=> id (("x", !!"X") @=> !!"Y") !!"f" !!"f'"
-    $ ("h" @-> "x" @-> "x'" @-> "x''" @-> (!!"h" $ !!"x" $ !!"x'" $ !!"x''")
-      <:> ("", id (("x", !!"X") @=> !!"Y") !!"f" !!"f'")
-          @=> ("x", !!"X")
-          @=> ("x'", !!"X")
-          @=> ("x''", id !!"X" !!"x" !!"x'")
-          @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'")
-      $ !!"p"))
+  let idff_roundtrip, _ =
+    synth
+      "((k x x' x'' ↦ k x x' x'') : ((x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x')) → Id (X → Y) f f')
+       (((h x x' x'' ↦ h x x' x'') : Id (X → Y) f f' → (x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x')) p)"
+  in
 
-let () = equal_at idff_roundtrip p idff
+  equal_at idff_roundtrip p idff;
 
-let idff'_roundtrip, _ =
-  synth
-    ("h" @-> "x" @-> "x'" @-> "x''" @-> (!!"h" $ !!"x" $ !!"x'" $ !!"x''")
-    <:> ("", id (("x", !!"X") @=> !!"Y") !!"f" !!"f'")
-        @=> ("x", !!"X")
-        @=> ("x'", !!"X")
-        @=> ("x''", id !!"X" !!"x" !!"x'")
-        @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'")
-    $ ("k" @-> "x" @-> "x'" @-> "x''" @-> (!!"k" $ !!"x" $ !!"x'" $ !!"x''")
-      <:> ( "",
-            ("x", !!"X")
-            @=> ("x'", !!"X")
-            @=> ("x''", id !!"X" !!"x" !!"x'")
-            @=> id !!"Y" (!!"f" $ !!"x") (!!"f'" $ !!"x'") )
-          @=> id (("x", !!"X") @=> !!"Y") !!"f" !!"f'"
-      $ !!"q"))
+  let idff'_roundtrip, _ =
+    synth
+      "((h x x' x'' ↦ h x x' x'') : Id (X → Y) f f' → (x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x'))
+       (((k x x' x'' ↦ k x x' x'') : ((x:X) (x':X) (x'':Id X x x') → Id Y (f x) (f' x')) → Id (X → Y) f f') q)"
+  in
 
-let () = equal_at idff'_roundtrip q idff'
+  equal_at idff'_roundtrip q idff';
 
-(* Identity types are invariant under eta-expansion *)
-let idff_eta, _ = synth (id (("x", !!"X") @=> !!"Y") ("x" @-> (!!"f" $ !!"x")) !!"f'")
-let () = equal idff idff_eta
+  (* Identity types are invariant under eta-expansion *)
+  let idff_eta, _ = synth "Id (X → Y) (x ↦ f x) f'" in
+  equal idff idff_eta;
 
-(* Ap is functorial *)
-let reflg, _ = synth (refl !!"g")
-let reflf_x2, _ = synth (refl !!"f" $ !!"x0" $ !!"x1" $ !!"x2")
-let () = unsynth (refl !!"g" $ !!"x0" $ !!"x1" $ !!"x2")
+  (* Ap is functorial *)
+  let reflg, _ = synth "refl g" in
+  let reflf_x2, _ = synth "refl f x0 x1 x2" in
+  unsynth "refl g x0 x1 x2";
 
-let reflg_reflf_x2, _ =
-  synth (refl !!"g" $ (!!"f" $ !!"x0") $ (!!"f" $ !!"x1") $ (refl !!"f" $ !!"x0" $ !!"x1" $ !!"x2"))
+  let reflg_reflf_x2, _ = synth "refl g (f x0) (f x1) (refl f x0 x1 x2)" in
 
-let refl_gof_x2, _ =
-  synth
-    (refl ("x" @-> (!!"g" $ (!!"f" $ !!"x")) <:> ("x", !!"X") @=> !!"Z") $ !!"x0" $ !!"x1" $ !!"x2")
+  let refl_gof_x2, _ = synth "refl ((x ↦ g (f x)) : X → Z) x0 x1 x2" in
 
-let () = equal reflg_reflf_x2 refl_gof_x2
+  equal reflg_reflf_x2 refl_gof_x2;
 
-(* The two degenerate squares associated to an identification have unequal types. *)
-let r1x2, r1x2ty = synth (refl !!"x2")
+  (* The two degenerate squares associated to an identification have unequal types. *)
+  let r1x2, r1x2ty = synth "refl x2" in
 
-let r2x2, r2x2ty =
-  synth
-    (refl ("x" @-> refl !!"x" <:> ("x", !!"X") @=> id !!"X" !!"x" !!"x") $ !!"x0" $ !!"x1" $ !!"x2")
+  let r2x2, r2x2ty = synth "refl ((x ↦ refl x) : (x:X) → Id X x x) x0 x1 x2" in
 
-let () = unequal r1x2ty r2x2ty
+  unequal r1x2ty r2x2ty;
 
-(* But applying symmetry identifies both their types and values. *)
-let sr1x2, sr1x2ty = synth (sym (refl !!"x2"))
-let () = equal sr1x2ty r2x2ty
-let () = equal sr1x2 r2x2
+  (* But applying symmetry identifies both their types and values. *)
+  let sr1x2, sr1x2ty = synth "sym (refl x2)" in
+  equal sr1x2ty r2x2ty;
+  equal sr1x2 r2x2;
 
-(* But the two degenerate squares associated to a reflexivity *are* equal. *)
-let r1reflx0, r1reflx0ty = synth (refl (refl !!"x0"))
+  (* But the two degenerate squares associated to a reflexivity *are* equal. *)
+  let r1reflx0, r1reflx0ty = synth "refl (refl x0)" in
 
-let r2reflx0, r2reflx0ty =
-  synth
-    (refl ("x" @-> refl !!"x" <:> ("x", !!"X") @=> id !!"X" !!"x" !!"x")
-    $ !!"x0"
-    $ !!"x0"
-    $ refl !!"x0")
+  let r2reflx0, r2reflx0ty = synth "refl ((x ↦ refl x) : (x:X) → Id X x x) x0 x0 (refl x0)" in
 
-let () = equal r1reflx0ty r2reflx0ty
-let () = equal r1reflx0 r2reflx0
+  equal r1reflx0ty r2reflx0ty;
+  equal r1reflx0 r2reflx0;
 
-(* Doubly-degenerate squares are also fixed by symmetry *)
-let sr1reflx0, _ = synth (sym (refl (refl !!"x0")))
-let () = equal r1reflx0 sr1reflx0
+  (* Doubly-degenerate squares are also fixed by symmetry *)
+  let sr1reflx0, _ = synth "sym (refl (refl x0))" in
+  equal r1reflx0 sr1reflx0;
+
+  ()

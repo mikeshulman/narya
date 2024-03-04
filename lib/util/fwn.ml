b48f1740ab66d82a7d3e690d0e968f64f1214c6b
Michael Shulman <shulman@sandiego.edu>
2024-03-03 22:57:26 -0800

    associativity of adding forwards and backwards nats

--- lib/util/fwn.ml
+++ lib/util/fwn.ml
@@ -18,13 +18,30 @@ let rec bplus_right : type a b ab. (a, b, ab) bplus -> b t = function
   | Suc ab -> Suc (bplus_right ab)
 
 (* We can also get a forwards one as the result.  This is the length-level analogue of prepending a backwards list on the front of a forwards one.  *)
-
 type (_, _, _) fplus =
   | Zero : (N.zero, 'a, 'a) fplus
   | Suc : ('a, 'b suc, 'ab) fplus -> ('a N.suc, 'b, 'ab) fplus
 
-(* Convert a backwards nat to a forwards one. *)
+type (_, _) has_fplus = Fplus : ('a, 'b, 'ab) fplus -> ('a, 'b) has_fplus
 
+let rec fplus : type a b. a N.t -> b t -> (a, b) has_fplus =
+ fun a b ->
+  match a with
+  | Nat Zero -> Fplus Zero
+  | Nat (Suc a) ->
+      let (Fplus ab) = fplus (Nat a) (Suc b) in
+      Fplus (Suc ab)
+
+(* These two kinds of addition associate with addition of backwards nats.  Here a, b, ab, and abc are backwards, while c and bc are forwards. *)
+let rec assocr :
+    type a b ab c bc abc.
+    (a, b, ab) N.plus -> (b, c, bc) fplus -> (ab, c, abc) bplus -> (a, bc, abc) bplus =
+ fun ab bc abc ->
+  match (ab, bc) with
+  | Zero, Zero -> abc
+  | Suc ab, Suc bc -> assocr ab bc (Suc abc)
+
+(* Convert a backwards nat to a forwards one. *)
 type _ of_bwn = Of_bwn : 'a t * (N.zero, 'a, 'b) bplus -> 'b of_bwn
 
 let of_bwn : type c. c N.t -> c of_bwn =
@@ -36,6 +53,7 @@ let of_bwn : type c. c N.t -> c of_bwn =
     | Nat (Suc a) -> go (Nat a) (Suc b) (Suc abc) in
   go c Zero Zero
 
+(* Compare two forwards nats *)
 let rec compare : type a b. a t -> b t -> (a, b) Monoid.compare =
  fun a b ->
   match (a, b) with
@@ -46,6 +64,7 @@ let rec compare : type a b. a t -> b t -> (a, b) Monoid.compare =
       | Neq -> Neq)
   | _ -> Neq
 
+(* Convert to an integer *)
 let rec to_int : type a. a t -> int = function
   | Zero -> 0
   | Suc a -> to_int a + 1

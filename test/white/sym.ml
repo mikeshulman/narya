ff58f17024a2ee9ef877df1c0ae5441d5dd93e93
Michael Shulman <shulman@sandiego.edu>
2024-03-08 01:15:17 -0800

    use 'run' in test/sym and test/symsym

--- test/white/sym.ml
+++ test/white/sym.ml
@@ -1,106 +1,109 @@
 open Testutil.Pmp
 
-let uu, _ = synth UU
-let xx = assume "X" uu
-let x00 = assume "x00" xx
-let x01 = assume "x01" xx
-let xx02, _ = synth (id !!"X" !!"x00" !!"x01")
-let x02 = assume "x02" xx02
-let x10 = assume "x10" xx
-let x11 = assume "x11" xx
-let xx12, _ = synth (id !!"X" !!"x10" !!"x11")
-let x12 = assume "x12" xx12
-let xx20, _ = synth (id !!"X" !!"x00" !!"x10")
-let xx21, _ = synth (id !!"X" !!"x01" !!"x11")
-let x20 = assume "x20" xx20
-let x21 = assume "x21" xx21
+let () =
+  run @@ fun () ->
+  let uu, _ = synth UU in
+  let xx = assume "X" uu in
+  let x00 = assume "x00" xx in
+  let x01 = assume "x01" xx in
+  let xx02, _ = synth (id !!"X" !!"x00" !!"x01") in
+  let x02 = assume "x02" xx02 in
+  let x10 = assume "x10" xx in
+  let x11 = assume "x11" xx in
+  let xx12, _ = synth (id !!"X" !!"x10" !!"x11") in
+  let x12 = assume "x12" xx12 in
+  let xx20, _ = synth (id !!"X" !!"x00" !!"x10") in
+  let xx21, _ = synth (id !!"X" !!"x01" !!"x11") in
+  let x20 = assume "x20" xx20 in
+  let x21 = assume "x21" xx21 in
 
-(* We can't apply symmetry to 0- or 1-dimensional things *)
-let () = unsynth (sym !!"x00")
-let () = unsynth (sym !!"x02")
+  (* We can't apply symmetry to 0- or 1-dimensional things *)
+  let () = unsynth (sym !!"x00") in
+  let () = unsynth (sym !!"x02") in
 
-let xx22, _ =
-  synth
-    (refl (refl !!"X")
-    $ !!"x00"
-    $ !!"x01"
-    $ !!"x02"
-    $ !!"x10"
-    $ !!"x11"
-    $ !!"x12"
-    $ !!"x20"
-    $ !!"x21")
+  let xx22, _ =
+    synth
+      (refl (refl !!"X")
+      $ !!"x00"
+      $ !!"x01"
+      $ !!"x02"
+      $ !!"x10"
+      $ !!"x11"
+      $ !!"x12"
+      $ !!"x20"
+      $ !!"x21") in
 
-let x22 = assume "x22" xx22
+  let x22 = assume "x22" xx22 in
 
-(* We can apply symmetry to a square, and the result has a different type in general. *)
-let sx22, sxx22 = synth (sym !!"x22")
-let () = unequal xx22 sxx22
+  (* We can apply symmetry to a square, and the result has a different type in general. *)
+  let sx22, sxx22 = synth (sym !!"x22") in
+  let () = unequal xx22 sxx22 in
 
-(* Its type has transposed boundary *)
-let sxx22', _ =
-  synth
-    (refl (refl !!"X")
-    $ !!"x00"
-    $ !!"x10"
-    $ !!"x20"
-    $ !!"x01"
-    $ !!"x11"
-    $ !!"x21"
-    $ !!"x02"
-    $ !!"x12")
+  (* Its type has transposed boundary *)
+  let sxx22', _ =
+    synth
+      (refl (refl !!"X")
+      $ !!"x00"
+      $ !!"x10"
+      $ !!"x20"
+      $ !!"x01"
+      $ !!"x11"
+      $ !!"x21"
+      $ !!"x02"
+      $ !!"x12") in
 
-let () = equal sxx22 sxx22'
+  let () = equal sxx22 sxx22' in
 
-(* Symmetry is an involution *)
-let ssx22, ssxx22 = synth (sym (sym !!"x22"))
-let () = equal ssxx22 xx22
-let () = equal ssx22 x22
+  (* Symmetry is an involution *)
+  let ssx22, ssxx22 = synth (sym (sym !!"x22")) in
+  let () = equal ssxx22 xx22 in
+  let () = equal ssx22 x22 in
 
-(* The action of functions on squares preserves symmetry *)
-let yy = assume "Y" uu
-let xtoy, _ = synth (("x", !!"X") @=> !!"Y")
-let f = assume "f" xtoy
+  (* The action of functions on squares preserves symmetry *)
+  let yy = assume "Y" uu in
+  let xtoy, _ = synth (("x", !!"X") @=> !!"Y") in
+  let f = assume "f" xtoy in
 
-let fx22, fx22ty =
-  synth
-    (refl (refl !!"f")
-    $ !!"x00"
-    $ !!"x01"
-    $ !!"x02"
-    $ !!"x10"
-    $ !!"x11"
-    $ !!"x12"
-    $ !!"x20"
-    $ !!"x21"
-    $ !!"x22")
+  let fx22, fx22ty =
+    synth
+      (refl (refl !!"f")
+      $ !!"x00"
+      $ !!"x01"
+      $ !!"x02"
+      $ !!"x10"
+      $ !!"x11"
+      $ !!"x12"
+      $ !!"x20"
+      $ !!"x21"
+      $ !!"x22") in
 
-let sfx22, sfx22ty =
-  synth
-    (sym
-       (refl (refl !!"f")
-       $ !!"x00"
-       $ !!"x01"
-       $ !!"x02"
-       $ !!"x10"
-       $ !!"x11"
-       $ !!"x12"
-       $ !!"x20"
-       $ !!"x21"
-       $ !!"x22"))
+  let sfx22, sfx22ty =
+    synth
+      (sym
+         (refl (refl !!"f")
+         $ !!"x00"
+         $ !!"x01"
+         $ !!"x02"
+         $ !!"x10"
+         $ !!"x11"
+         $ !!"x12"
+         $ !!"x20"
+         $ !!"x21"
+         $ !!"x22")) in
 
-let fsx22, fsx22ty =
-  synth
-    (refl (refl !!"f")
-    $ !!"x00"
-    $ !!"x10"
-    $ !!"x20"
-    $ !!"x01"
-    $ !!"x11"
-    $ !!"x21"
-    $ !!"x02"
-    $ !!"x12"
-    $ sym !!"x22")
+  let fsx22, fsx22ty =
+    synth
+      (refl (refl !!"f")
+      $ !!"x00"
+      $ !!"x10"
+      $ !!"x20"
+      $ !!"x01"
+      $ !!"x11"
+      $ !!"x21"
+      $ !!"x02"
+      $ !!"x12"
+      $ sym !!"x22") in
 
-let () = equal sfx22ty fsx22ty
-let () = equal sfx22 fsx22
+  let () = equal sfx22ty fsx22ty in
+  let () = equal sfx22 fsx22 in
+  ()

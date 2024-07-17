74d1207c3dcbf46280e0ef6f3b6251421604dc35
Michael Shulman <shulman@sandiego.edu>
2024-07-16 23:34:37 -0700

    intrinsically well-typed total pbij maps

--- lib/dim/pbij.ml
+++ lib/dim/pbij.ml
@@ -143,3 +143,189 @@ let deg_comp_pbij : type m n i. (m, n) deg -> (m, i) pbij -> (n, i) deg_comp_pbi
  fun d (Pbij (ins, shuf)) ->
   let (DCP (ins, shuf, s)) = deg_comp_pbij_internal d ins shuf in
   Deg_comp_pbij (Pbij (ins, shuf), s)
+
+module rec Pbijmap : sig
+  module Param : sig
+    type (_, _) t =
+      | Wrap : ('evaluation, 'intrinsic, 'v) Pbijmap.t -> ('evaluation, 'intrinsic * 'v) t
+  end
+
+  module Tup : module type of Tuple.Make (Param)
+
+  type (_, _, _) t =
+    | Zero : 'v -> ('evaluation, D.zero, 'v) t
+    | Suc : {
+        left : ('evaluation, 'intrinsic, 'v) t;
+        right : ('evaluation, 'intrinsic * 'v) Tup.t;
+      }
+        -> ('evaluation, 'intrinsic D.suc, 'v) t
+
+  val intrinsic : ('evaluation, 'intrinsic, 'v) t -> 'intrinsic D.t
+
+  type (_, _) wrapped = Wrap : ('evaluation, 'intrinsic, 'v) t -> ('evaluation, 'v) wrapped
+
+  val find : ('evaluation, 'intrinsic) pbij -> ('evaluation, 'intrinsic, 'v) t -> 'v
+  val find_singleton : ('evaluation, 'intrinsic, 'v) t -> 'v option
+
+  val set :
+    ('evaluation, 'intrinsic) pbij ->
+    'v ->
+    ('evaluation, 'intrinsic, 'v) t ->
+    ('evaluation, 'intrinsic, 'v) t
+
+  val build :
+    'evaluation D.t ->
+    'intrinsic D.t ->
+    (('evaluation, 'intrinsic) pbij -> 'v) ->
+    ('evaluation, 'intrinsic, 'v) t
+
+  val singleton : 'v -> ('evaluation, D.zero, 'v) t
+  val map : ('v -> 'w) -> ('evaluation, 'intrinsic, 'v) t -> ('evaluation, 'intrinsic, 'w) t
+
+  val iter :
+    'evaluation D.t ->
+    (('evaluation, 'intrinsic) pbij -> 'v -> unit) ->
+    ('evaluation, 'intrinsic, 'v) t ->
+    unit
+
+  val fold :
+    'evaluation D.t ->
+    (('evaluation, 'intrinsic) pbij -> 'v -> 'acc -> 'acc) ->
+    ('evaluation, 'intrinsic, 'v) t ->
+    'acc ->
+    'acc
+end = struct
+  module Param = struct
+    type (_, _) t =
+      | Wrap : ('evaluation, 'intrinsic, 'v) Pbijmap.t -> ('evaluation, 'intrinsic * 'v) t
+  end
+
+  module Tup = Tuple.Make (Param)
+
+  type (_, _, _) t =
+    | Zero : 'v -> ('evaluation, D.zero, 'v) t
+    | Suc : {
+        left : ('evaluation, 'intrinsic, 'v) t;
+        right : ('evaluation, 'intrinsic * 'v) Tup.t;
+      }
+        -> ('evaluation, 'intrinsic D.suc, 'v) t
+
+  let rec intrinsic : type evaluation intrinsic v. (evaluation, intrinsic, v) t -> intrinsic D.t =
+    function
+    | Zero _ -> D.zero
+    | Suc { left; _ } -> D.suc (intrinsic left)
+
+  type (_, _) wrapped = Wrap : ('evaluation, 'intrinsic, 'v) t -> ('evaluation, 'v) wrapped
+
+  let rec find :
+      type evaluation intrinsic v. (evaluation, intrinsic) pbij -> (evaluation, intrinsic, v) t -> v
+      =
+   fun p m ->
+    match (p, m) with
+    | Pbij (Zero _, Zero), Zero v -> v
+    | Pbij (ins, Left shuf), Suc m -> find (Pbij (ins, shuf)) m.left
+    | Pbij (Suc (ins, i), Right shuf), Suc m ->
+        let (Wrap m) = Tup.find i m.right in
+        find (Pbij (ins, shuf)) m
+
+  let rec set :
+      type evaluation intrinsic v.
+      (evaluation, intrinsic) pbij ->
+      v ->
+      (evaluation, intrinsic, v) t ->
+      (evaluation, intrinsic, v) t =
+   fun p v m ->
+    match (p, m) with
+    | Pbij (Zero _, Zero), Zero _ -> Zero v
+    | Pbij (ins, Left shuf), Suc m -> Suc { m with left = set (Pbij (ins, shuf)) v m.left }
+    | Pbij (Suc (ins, i), Right shuf), Suc m ->
+        Suc
+          {
+            m with
+            right = Tup.update i (fun (Wrap m) -> Wrap (set (Pbij (ins, shuf)) v m)) m.right;
+          }
+
+  let find_singleton : type evaluation intrinsic v. (evaluation, intrinsic, v) t -> v option =
+    function
+    | Zero v -> Some v
+    | Suc _ -> None
+
+  let rec build :
+      type evaluation intrinsic v.
+      evaluation D.t ->
+      intrinsic D.t ->
+      ((evaluation, intrinsic) pbij -> v) ->
+      (evaluation, intrinsic, v) t =
+   fun evaluation intrinsic f ->
+    match intrinsic with
+    | Nat Zero -> Zero (f (Pbij (ins_zero evaluation, Zero)))
+    | Nat (Suc intrinsic) ->
+        Suc
+          {
+            left =
+              build evaluation (Nat intrinsic) (fun (Pbij (ins, shuf)) -> f (Pbij (ins, Left shuf)));
+            right =
+              Tup.build evaluation
+                {
+                  build =
+                    (fun i ->
+                      Wrap
+                        (build (D.insert_in evaluation i) (Nat intrinsic) (fun (Pbij (ins, shuf)) ->
+                             f (Pbij (Suc (ins, i), Right shuf)))));
+                };
+          }
+
+  let singleton : type evaluation v. v -> (evaluation, D.zero, v) t = fun v -> Zero v
+
+  let rec map :
+      type evaluation intrinsic v w.
+      (v -> w) -> (evaluation, intrinsic, v) t -> (evaluation, intrinsic, w) t =
+   fun f m ->
+    match m with
+    | Zero v -> Zero (f v)
+    | Suc { left; right } ->
+        Suc { left = map f left; right = Tup.map { map = (fun (Wrap x) -> Wrap (map f x)) } right }
+
+  let rec iter :
+      type evaluation intrinsic v.
+      evaluation D.t ->
+      ((evaluation, intrinsic) pbij -> v -> unit) ->
+      (evaluation, intrinsic, v) t ->
+      unit =
+   fun evaluation f m ->
+    match m with
+    | Zero v -> f (Pbij (ins_zero evaluation, Zero)) v
+    | Suc { left; right } ->
+        iter evaluation (fun (Pbij (ins, shuf)) -> f (Pbij (ins, Left shuf))) left;
+        Tup.iter
+          {
+            it =
+              (fun i (Wrap x) ->
+                iter (D.insert_in evaluation i)
+                  (fun (Pbij (ins, shuf)) -> f (Pbij (Suc (ins, i), Right shuf)))
+                  x);
+          }
+          right
+
+  let rec fold :
+      type evaluation intrinsic v acc.
+      evaluation D.t ->
+      ((evaluation, intrinsic) pbij -> v -> acc -> acc) ->
+      (evaluation, intrinsic, v) t ->
+      acc ->
+      acc =
+   fun evaluation f m acc ->
+    match m with
+    | Zero v -> f (Pbij (ins_zero evaluation, Zero)) v acc
+    | Suc { left; right } ->
+        let acc = fold evaluation (fun (Pbij (ins, shuf)) -> f (Pbij (ins, Left shuf))) left acc in
+        Tup.fold
+          {
+            fold =
+              (fun i (Wrap x) acc ->
+                fold (D.insert_in evaluation i)
+                  (fun (Pbij (ins, shuf)) -> f (Pbij (Suc (ins, i), Right shuf)))
+                  x acc);
+          }
+          right acc
+end

a2e3bf1885ee89f3b7eb0f62746569ab1da6f492
Michael Shulman <shulman@sandiego.edu>
2025-03-17 02:06:06 -0700

    link the basic compunit with itself

--- lib/top/execute.ml
+++ lib/top/execute.ml
@@ -154,22 +154,23 @@ let rec unmarshal (compunit : Compunit.t) (lookup : FilePath.filename -> Compuni
             let table = Hashtbl.create 20 in
             Mbwd.miter (fun [ (i, ifile) ] -> Hashtbl.add table i (lookup ifile)) [ old_imports ];
             Hashtbl.add table old_compunit compunit;
-            let find_in_table n x =
-              Hashtbl.find_opt table x
-              <|> Anomaly ("missing compunit while loading compiled file: part " ^ n) in
+            let find_in_table x =
+              if x = Compunit.basic then Compunit.basic
+              else
+                Hashtbl.find_opt table x
+                <|> Anomaly "missing compunit while unmarshaling compiled file" in
             (* Now we load the definitions from the compiled file, replacing all the old compunits by the new ones. *)
-            Global.from_channel_unit (find_in_table "1") chan compunit;
+            Global.from_channel_unit find_in_table chan compunit;
             let trie =
               Trie.map
                 (fun _ (data, tag) ->
                   match data with
-                  | `Constant c, loc ->
-                      ((`Constant (Constant.remake (find_in_table "2") c), loc), tag)
+                  | `Constant c, loc -> ((`Constant (Constant.remake find_in_table c), loc), tag)
                   | `Notation (User.User u), loc ->
                       (* We also have to re-make the notation objects since they contain constant names (print keys) and their own autonumbers (but those are only used for comparison locally so don't need to be walked elsewhere). *)
                       let key =
                         match u.key with
-                        | `Constant c -> `Constant (Constant.remake (find_in_table "3") c)
+                        | `Constant c -> `Constant (Constant.remake find_in_table c)
                         | `Constr (c, i) -> `Constr (c, i) in
                       let u = User.User { u with key } in
                       ((`Notation (u, make_user u), loc), tag))
